0ede7ac7526c34277833c4be885664acd77c37da
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-05-30 13:19:34 +0430

    #1 Hello World!

--- app/src/main/java/com/jmzd/ghazal/kotlinTutorialNouri/Main.kt
+++
@@ -1,5 +0,0 @@
-package com.jmzd.ghazal.kotlinTutorialNouri
-
-fun main(){
-    print("Hello World!")
-}
---
+++ app/src/main/java/com/jmzd/ghazal/kotlinTutorialNouri/Print.kt
@@ -0,0 +1,12 @@
+package com.jmzd.ghazal.kotlinTutorialNouri
+
+fun main(){
+    print("Hello World!")
+    print("Ghazal")
+    //Hello World!Ghazal
+
+    println("Hello World!")
+    print("Ghazal") //enter mizane
+    //Hello World!
+    //Ghazal
+}
