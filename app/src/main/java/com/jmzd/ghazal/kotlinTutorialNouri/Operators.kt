e17f27cba5cc9a3a2bdc00e1d8d6402709faeff6
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2024-05-30 13:45:46 +0430

    #3 operators

---
+++ app/src/main/java/com/jmzd/ghazal/kotlinTutorialNouri/Operators.kt
@@ -0,0 +1,20 @@
+package com.jmzd.ghazal.kotlinTutorialNouri
+
+fun main() {
+    val number1 = 10
+    val number2 = 2
+    val sum = number1 + number2
+    println(sum) //12
+
+    val number3 = 10f
+    val number4 = 2f
+    val sum2 = number3 + number4
+    println(sum2) //12.0
+
+    val number5 = "5"
+    val number6 = 3
+    val sum3 = number5 + number6
+    println(sum3) //53
+
+
+}
